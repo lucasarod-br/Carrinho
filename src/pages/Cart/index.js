804848bbfc3e4c2f874e7ad986297bd1a8d0784a
Lucas <lalves547@gmail.com>
2023-02-19 15:17:14 -0300

    App com cra, redux, router-dom, axios e page cart

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
+import './Cart.css'
+
+
+function Cart() {
+  const url = 'd6e9a93f-9741-4494-b81e-637a8e9b8ddd'
+  const [itens, setItens] = useState([])
+  useEffect(()=> {
+    api.get(url)
+    .then( (data) => setItens(data.data.items))
+  }, [])
+  return (
+    <div>
+        <div className='elements'>
+        {itens.map((iten) => <li>{iten.product.name}</li>)}
+        </div>
+        <div>Prices</div>
+        <button>Next</button>
+    </div>
+  )
+}
+
+export default Cart
