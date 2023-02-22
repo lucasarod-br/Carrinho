39f80c51705026c8b072e26f72a0df9cab061451
Lucas <lalves547@gmail.com>
2023-02-22 14:34:44 -0300

    Criando componente Container

---
+++ src/components/Container/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    
+    max-width: 680px; 
+    padding: 2rem 1rem;
+    border: 1px solid #CCCCCC;
+    border-radius: 4px;
+    background-color: #fff;
+    margin: auto 4px;
+`
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,40 +1,28 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import styled from "styled-components";
-import Product from "./Product";
+  import { Container } from "components/Container";
+  import React from "react";
+  import { useSelector } from "react-redux";
+  import Product from "./Product";
 
-const StyledProducts = styled.div`
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-    width: 95vw;
-    max-width: 680px; 
-    padding: 2rem 1rem;
-    border: 1px solid #CCCCCC;
-    border-radius: 4px;
-    background-color: #fff;
-    margin: auto auto;
-`
 
-function Products({ price }) {
-  const cartItems = useSelector((state) => state.cart.items);
 
-  return (
-    <StyledProducts>
-      {cartItems.map((iten) => (
-        <Product 
-            key={iten.product.sku} 
-            img={iten.product.imageObjects[0].thumbnail}
-            name={iten.product.name}
-            price={price}
-            maxPrice={price && iten.product.priceSpecification.maxPrice}
-            originalPrice={price && iten.product.priceSpecification.originalPrice}
-            
-            />
-      ))}
-    </StyledProducts>
-  );
-}
+  function Products({ price }) {
+    const cartItems = useSelector((state) => state.cart.items);
 
-export default Products;
+    return (
+      <Container>
+        {cartItems.map((iten) => (
+          <Product 
+              key={iten.product.sku} 
+              img={iten.product.imageObjects[0].thumbnail}
+              name={iten.product.name}
+              price={price}
+              maxPrice={price && iten.product.priceSpecification.maxPrice}
+              originalPrice={price && iten.product.priceSpecification.originalPrice}
+              
+              />
+        ))}
+      </Container>
+    );
+  }
+
+  export default Products;
